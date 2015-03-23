ec8d29e68189e554969e60f836c260ad4bfe34e1
Niall Clancy <Niall.Clancy@lottery.ie>
2015-03-23 11:56:56 +0000

    Elevator MKVII

--- ClancyElevator.py
+++ ClancyElevator.py
@@ -4,31 +4,29 @@ class Building(object):
     def __init__ (self, num_floors, customers_for_boarding_list):        
         self.num_floors = num_floors
         self.boarding = customers_for_boarding_list
-        self.elevator = Elevator(self, num_floors)            
+        self.elevator = Elevator(self, num_floors)
 
     def run(self, elevator):
-        while self.boarding or  self.elevator.passenger_list:
-            #accept any passengers waiting at this floor from the building boarding_list
-            #goes onto the (Elevator) Passenger List - Embarking
-            for customer in self.boarding:
-                self.elevator.embarking_customer()
-                print('Customer Embarked')
-            #Allow customers off if required floor matches that in Passenger List - Disembarking
-            for customer in self.elevator.passenger_list:
-                self.elevator.disembarking_customer()
-                print('Customer Disembarked')
+        while self.elevator.passenger_list or  self.boarding:
+            '''accept any passengers waiting at this floor from the building boarding_list
+            goes onto the (Elevator) Passenger List - Embarking'''
+            elevator.embarking_customer()
+            #print('Customer Embarked')
+            #Allow customers off if required floor matches that in Passenger List - 
+            elevator.disembarking_customer()
+            #print('Customer Disembarked')
            
-            self.elevator.move()
+            elevator.move()
     
         if len(self.boarding) == len(self.elevator.passenger_list):
-            print('\nNo more customers on Board.  All Customers taken care of.  \n\nThank you ')
+            print('\nNo more customers on Board.  All Customers taken care of.  \n\nThank you for using DT249 Elevators.')
             #customer.output()
 
 class Elevator(object):
     def __init__(self, num_floors, customers_for_boarding_list):
-        self.num_floors = num_floors
-        self.customers_for_boarding_list = customers_for_boarding_list
+        self.floors = num_floors
         self.passenger_list = []
+        self.customers_for_boarding_list = customers_for_boarding_list
         self.current_floor = 0
         self.embarked_customer = 0
         self.disembarked_customer = 0
@@ -36,34 +34,38 @@ class Elevator(object):
 
 #customer moves from customers_for_boarding_list and embarks onto (Elevator)passenger_list
     def embarking_customer(self):
-        print('Doors Open ... ')
         for customer in self.customers_for_boarding_list:
             if customer.starting_floor == self.current_floor:
                 self.passenger_list.append(customer)
-                self.customers.for_boarding_list.remove(customer)
+                self.customers_for_boarding_list.remove(customer)
                 self.embarked_customer += 1
-                print("Customer with ID: {}".format(customer.ID) + " gets off at floor {}".format(customer.ending_floor))
-                print('Customer Embarked')
+                print('Customer Alighting . . . Doors Open ... ')
+                print("Customer with ID: {}".format(customer.ID) + " gets on at floor {}".format(customer.starting_floor-1))
+                # + "& presses the button for floor:"item.ending_floor)
+                print('... Customer embarked ...')
+                print('... Doors Close ...')
 
 #customer exits lift and is removed from (Elevator)passenger_list
     def disembarking_customer(self):
-        print('Doors Open ...')
-        for customer in self.customers_for_boarding_list:
+        for customer in self.passenger_list:
             if customer.ending_floor == self.current_floor:
                 self.passenger_list.remove(customer)
                 self.disembarked_customer += 1
+                print('Customer Disembarking . . . Doors Open ...')
                 print("Customer with ID: {}".format(customer.ID) + " gets off at floor {}".format(customer.ending_floor))
-                print('Customer disembarked')                
+                print('... Customer disembarked ...')
+                print('... Doors Close ...')
 
 #elevator moves one floor at a time
     def move(self):
+        print('Elevator is on floor {}'.format(self.current_floor))
         '''Move Elevator one floor at a time'''
-        if self.direction == 'up' and self.current_floor == self.num_floors:
+        if self.direction == 'up' and self.current_floor == self.floors:
                 self.direction = 'down'
         else:
             self.direction = 'up'
 
-        if self.current_floor != self.num_floors:
+        if self.current_floor != self.floors:
             self.current_floor += 1
             print('Elevator moving up one floor')
             print('\nCurrent floor {}'.format(self.current_floor))
@@ -113,9 +115,9 @@ def main():
 
     while True:
         try:
-            num_floors = int(input("Please enter the number of floors in the Building: "))
+            floor_amount = int(input("Please enter the number of floors in the Building: "))
             num_customers = int(input("Please enter the number of customers in the Building:"))
-            if num_floors == int(0):
+            if floor_amount == int(0):
                 raise ValueError
         except:
             print("Please enter an integer greater than zero")              # Catches user input for 0 floors
@@ -125,20 +127,18 @@ def main():
     boarding_list = []
 
     #building = Building(num_floors, boarding_list)
-
-    for cust_id in range(num_customers):
-        ID = cust_id+1
-        customer = Customer(cust_id, starting_floor=randint(1, num_floors), ending_floor=randint(1, num_floors))
+    cust_id = 0
+    for customer in range(0, num_customers):
+        customer = Customer(starting_floor=randint(1, floor_amount), ending_floor=randint(1, floor_amount), ID = cust_id)
         customer.output()
         boarding_list.append(customer)
+        cust_id += 1
         
     print('\nCustomers in List format: {}\n'.format(boarding_list))
         
-
-    elevator=Elevator(num_floors, boarding_list)
-    building = Building(num_floors, boarding_list)
+    building = Building(floor_amount, boarding_list)
+    elevator=Elevator(floor_amount, boarding_list)
     building.run(elevator)
-
 
 if __name__ == '__main__':
     main()
